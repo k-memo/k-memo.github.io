1fe76adccc5e6c2957d94d2c125a8f9d06a4e5a0
Mehmet K <fhs49041@fh-salzburg.ac.at>
2023-12-02 17:10:28 +0100

    added project tab with functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const dropdown = document.getElementById("dropdown");
   const dropdownToggle = document.getElementById("drop-toggle");
 
-  // Add a click event listener to each link in menu1
   const links = dropdown.querySelectorAll("a");
   links.forEach((link) => {
     link.addEventListener("click", () => {
-      // Close the dropdown by unchecking the toggle checkbox
       dropdownToggle.checked = false;
     });
   });
 });
+
+function toggleContent(element) {
+  let collapseIcon = element.querySelector("svg");
+
+  // Select all content elements
+  let allContents = document.querySelectorAll(".item-content");
+
+  allContents.forEach((item) => {
+    if (item !== element.nextElementSibling) {
+      item.classList.remove("active");
+    }
+  });
+
+  // Collapse active content and move the view
+  let content = element.nextElementSibling;
+
+  collapseIcon.classList.toggle("collapsed");
+  content.classList.toggle("active");
+
+  if (content.classList.contains("active")) {
+    setTimeout(() => {
+      content.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }, 500);
+  }
+}
